8f584ba040e1fcc289e8907d3bd1df574d4e08c1
Claus Guttesen <kometen@gmail.com>
2013-03-13 16:53:39 +0100

    Initial Commit

---
+++ EMVgraph/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EMVgraph
+//
+//  Created by Claus Guttesen on 13/03/13.
+//  Copyright (c) 2013 Claus Guttesen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
