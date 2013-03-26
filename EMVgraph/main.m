f63f8833270970475a5efe60dc6361a1fc1d9e9c
Claus Guttesen <kometen@gmail.com>
2013-03-26 14:46:31 +0100

    Added more nodes and edges.

--- EMVgraph/main.m
+++ EMVgraph/main.m
@@ -22,10 +22,16 @@ int main(int argc, const char * argv[]) {
         Node *node1 = [[Node alloc] initWithName:@"Claus Guttesen" andDescription:@"complete name"];
         Node *node2 = [[Node alloc] initWithName:@"Anne-Merete Kleppenes" andDescription:@"complete name"];
         Node *node3 = [[Node alloc] initWithName:@"Eline Clausdatter Kleppenes" andDescription:@"complete name"];
+        Node *node4 = [[Node alloc] initWithName:@"Leif Guttesen" andDescription:@"complete name"];
+        Node *node5 = [[Node alloc] initWithName:@"Ruth Guttesen" andDescription:@"complete name"];
+        Node *node6 = [[Node alloc] initWithName:@"Einar Kleppenes" andDescription:@"complete name"];
+        Node *node7 = [[Node alloc] initWithName:@"Alvhild Kleppenes" andDescription:@"complete name"];
         
         Edge *edge1 = [[Edge alloc] initWithName:@"wife" andDescription:@"married to"];
         Edge *edge2 = [[Edge alloc] initWithName:@"husbond" andDescription:@"married to"];
         Edge *edge3 = [[Edge alloc] initWithName:@"daughter" andDescription:@"daughter"];
+        Edge *edge4 = [[Edge alloc] initWithName:@"father" andDescription:@"father"];
+        Edge *edge5 = [[Edge alloc] initWithName:@"mother" andDescription:@"mother"];
         
         Row *row1 = [[Row alloc] initWithKey:@"firstname" andValue:@"Claus"];
         Row *row2 = [[Row alloc] initWithKey:@"lastname" andValue:@"Guttesen"];
@@ -36,14 +42,30 @@ int main(int argc, const char * argv[]) {
         Relation *relation1 = [[Relation alloc] initWithNode:node2 andEdge:edge1];
         Relation *relation2 = [[Relation alloc] initWithNode:node3 andEdge:edge3];
         Relation *relation3 = [[Relation alloc] initWithNode:node1 andEdge:edge2];
+        Relation *relation4 = [[Relation alloc] initWithNode:node4 andEdge:edge4];
+        Relation *relation5 = [[Relation alloc] initWithNode:node5 andEdge:edge5];
+        Relation *relation6 = [[Relation alloc] initWithNode:node6 andEdge:edge4];
+        Relation *relation7 = [[Relation alloc] initWithNode:node7 andEdge:edge5];
+        Relation *relation8 = [[Relation alloc] initWithNode:node1 andEdge:edge4];
         
         [nodeChain addObject:mnode];
         [nodeChain addObject:node1];
         [nodeChain addObject:node2];
         [nodeChain addObject:node3];
+        [nodeChain addObject:node4];
+        [nodeChain addObject:node5];
+        [nodeChain addObject:node6];
+        [nodeChain addObject:node7];
+        
         [node1 addRelation:relation1];
         [node1 addRelation:relation2];
         [node2 addRelation:relation3];
+        [node1 addRelation:relation4];
+        [node1 addRelation:relation5];
+        [node2 addRelation:relation6];
+        [node2 addRelation:relation7];
+        [node3 addRelation:relation8];
+        
         NSLog(@"node 1: name: %@, value: %@", node1.name, node1.description);
         for (Node *node in nodeChain) {
             NSLog(@"node-uuid: %@, node-name: %@", node.uuid, node.name);
